2fbbcb4ec0a89de5d587b2f9beed054fd143cf88
b1uey0 <3_14km@naver.com>
2023-11-16 12:00:18 +0900

    yeeun commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def concatenate_strings(str1, str2):
+    result = str1 + str2
+    return result
+
+# 예시 사용
+string1 = "Hello, "
+string2 = "world!"
+resulting_string = concatenate_strings(string1, string2)
+print(resulting_string)
