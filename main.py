9e449a1b21f033952c03758f3c2da6664d512dfd
jihyeon <alwozmb@naver.com>
2023-11-16 12:02:24 +0900

    jihyeon commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def check_even():
+    try:
+        # 사용자로부터 숫자 입력 받기
+        number = int(input("숫자를 입력하세요: "))
+        
+        # 입력한 숫자가 짝수인지 확인
+        if number % 2 == 0:
+            print(f"{number}은(는) 짝수입니다.")
+        else:
+            print(f"{number}은(는) 홀수입니다.")
+    
+    except ValueError:
+        print("유효한 숫자를 입력하세요.")
+
+# 함수 호출
+check_even()
